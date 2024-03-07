b889f05dcafbc884255cd69328f44ac9e07732f3
Amisha <amishasaini124@gmail.com>
2024-03-07 15:13:19 +0530

    Change

--- src/main/java/com/example/SportLogin/model/Role.java
+++ src/main/java/com/example/SportLogin/model/Role.java
@@ -1,24 +1,10 @@
 package com.example.SportLogin.model;
 import jakarta.persistence.*;
-import lombok.*;
 
-import java.util.Set;
 
-@Getter
-@Data
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "roles")
-public class Role {
-
-    @Id
-    @Column(name="id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "name")
-    private String name;
+public enum Role {
+    USER,
+    ADMIN
 
 }
+
